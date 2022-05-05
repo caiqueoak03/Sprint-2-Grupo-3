6b958288f4e62512c03a5281975f2f1b4dbe020b
Guilherme Benassi <guilherme.amaral@sptech.school>
2022-05-05 15:53:47 -0300

    reestrutura

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -33,7 +33,7 @@ function entrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
-        
+
         usuarioModel.entrar(email, senha)
             .then(
                 function (resultado) {
@@ -62,21 +62,31 @@ function entrar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nomeEmpresa = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var telFixo = req.body.telFixoServer;
+    var telCelular = req.body.telCelularServer;
+    var cnpj = req.body.cnpjServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (nomeEmpresa == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
-        
+    } else if (telFixo == undefined) {
+        res.status(400).send("Seu telefone fixo está undefined!");
+    } else if (telCelular == undefined) {
+        res.status(400).send("Sua telefone celular está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("Sua cnpj está undefined!");
+    }
+    else {
+
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nomeEmpresa, email, senha, telFixo, telCelular, cnpj)
             .then(
                 function (resultado) {
                     res.json(resultado);

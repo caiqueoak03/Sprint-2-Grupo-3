15e335b4ba7b2af447460f6df9f023b792f1ac2d
GuilhermeDelfino <guilherme.narciso@sptech.school>
2022-05-23 10:42:11 -0300

    adicionei o script do azure

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -13,13 +13,13 @@ CREATE DATABASE soyventure;
 USE soyventure;
 
 CREATE TABLE empresa (
-	idEmpresa int primary key AUTO_INCREMENT,
+	idEmpresa int primary key IDENTITY(1,1),
 	nome varchar(45) not null,
 	cnpj char(14) unique not null
 );
 
 CREATE TABLE funcionario (
-	idFuncionario int AUTO_INCREMENT,
+	idFuncionario int IDENTITY(1,1),
 	nome varchar(45) not null,
 	sobrenome varchar(45) not null,
 	email varchar(45) unique not null,
@@ -34,7 +34,7 @@ CREATE TABLE funcionario (
 );
 
 CREATE TABLE fazenda (
-	idFazenda INT PRIMARY KEY AUTO_INCREMENT,
+	idFazenda INT PRIMARY KEY IDENTITY(1,1),
 	nome varchar(45) not null,
 	cep char(8) unique not null,
 	areaHectare decimal(10,2) not null,
@@ -52,7 +52,7 @@ create table contrato (
 );
 
 create table setor (
-	idSetor int AUTO_INCREMENT,
+	idSetor int IDENTITY(1,1),
 	nome varchar(45) not null,
 	modeloSensor varchar(45) default 'HOBOnet T11',
 	longitudeSensor decimal(4,2),
@@ -63,7 +63,7 @@ create table setor (
 );
 
 create table dado (
-	idDado int AUTO_INCREMENT,
+	idDado int IDENTITY(1,1),
 	temperatura decimal(3,1),
 	umidade decimal(4,1),
 	dataDado date default(CURRENT_DATE),
@@ -81,24 +81,65 @@ create table dado (
 
 /* para sql server - remoto - produção */
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
+CREATE TABLE empresa (
+	idEmpresa int primary key IDENTITY(1,1),
+	nome varchar(45) not null,
+	cnpj char(14) unique not null
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+CREATE TABLE funcionario (
+	idFuncionario int IDENTITY(1,1),
+	nome varchar(45) not null,
+	sobrenome varchar(45) not null,
+	email varchar(45) unique not null,
+	senha varchar(45) not null,
+	cargo varchar(45)  not null, check (cargo in ('analista', 'supervisor', 'gerente')),
+	urlImg varchar(300),
+	telFixo char(10),
+	telCelular char(11),
+	fkEmpresa int,
+	foreign key (fkEmpresa) references empresa(idEmpresa),
+	primary key (idFuncionario, fkEmpresa)
+);
+
+CREATE TABLE fazenda (
+	idFazenda INT PRIMARY KEY IDENTITY(1,1),
+	nome varchar(45) not null,
+	cep char(8) unique not null,
+	areaHectare decimal(10,2) not null,
+	qtdSetores int  not null,
+	telFixo char(10),
+	telCelular char(11)
 ); 
 
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	temperatura DOUBLE,
-	umidade DOUBLE,
-	momento DATETIME,
-	fk_aquario INT
+create table contrato (
+	fkFuncionario int,
+	fkFazenda int,
+	foreign key (fkFuncionario) references funcionario(idFuncionario),
+	foreign key (fkFazenda) references fazenda(idFazenda),
+	primary key (fkFuncionario, fkFazenda)
+);
+
+create table setor (
+	idSetor int IDENTITY(1,1),
+	nome varchar(45) not null,
+	modeloSensor varchar(45) default 'HOBOnet T11',
+	longitudeSensor decimal(4,2),
+	latitudeSensor decimal(4,2),
+	fkFazenda int,
+	foreign key (fkFazenda) references fazenda(idFazenda),
+	primary key (idSetor, fkFazenda)
+);
+
+create table dado (
+	idDado int IDENTITY(1,1),
+	temperatura decimal(3,1),
+	umidade decimal(4,1),
+	dataDado date default(CURRENT_DATE),
+	tempoDado time default(CURRENT_TIME),
+	fkSetor int,
+	setor_fkFazenda int,
+	foreign key (fkSetor) references setor(idSetor),
+	foreign key (setor_fkFazenda) references setor(fkFazenda),
+	primary key (idDado, fkSetor, setor_fkFazenda)
 );

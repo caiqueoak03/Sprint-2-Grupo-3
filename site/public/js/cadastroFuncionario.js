4027e0ea4f3b2815a51eb5e527f2677c8094fc13
caiqueoak03 <caique.silva@sptech.school>
2022-05-29 15:17:50 -0300

    azure versão 14

--- site/public/js/cadastroFuncionario.js
+++ site/public/js/cadastroFuncionario.js
@@ -1,4 +1,47 @@
 function carregarFazendas() {
+	fetch("/usuarios/carregarFazendas", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			idFuncionarioServer: sessionStorage.ID_FUNCIONARIO,
+		}),
+	})
+		.then(function (resposta) {
+			console.log("ESTOU NO THEN DO carregarFazendas()!");
+
+			if (resposta.ok) {
+				resposta.json().then((json) => {
+					if (json[0].qtdFazendas == 0) {
+						return;
+					}
+					console.log("json: " + json);
+					console.log("JSON: " + JSON.stringify(json));
+
+					var idFazendas = [];
+					var nomeFazendas = [];
+
+					for (let i = 0; i < json.length; i++) {
+						nomeFazendas.push(json[i].nome);
+						idFazendas.push(json[i].idFazenda);
+					}
+
+					sessionStorage.ID_FAZENDAS = idFazendas;
+					sessionStorage.NOME_FAZENDAS = nomeFazendas;
+				});
+			} else {
+				console.log("Houve um erro ao tentar carregar as fazendas!");
+
+				resposta.text().then((texto) => {
+					console.error(texto);
+				});
+			}
+		})
+		.catch(function (erro) {
+			console.log(erro);
+		});
+
 	var len = sessionStorage.ID_FAZENDAS.split(",").length;
 	var idFazendas = sessionStorage.ID_FAZENDAS.split(",");
 	var nomeFazendas = sessionStorage.NOME_FAZENDAS.split(",");
@@ -12,4 +55,6 @@ function carregarFazendas() {
               <input type="checkbox" class="boxes" id='${idFazendas[i]}'/>
             </li>`;
 	}
+
+	return false;
 }
